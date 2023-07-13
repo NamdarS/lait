b6896764a94ad29f4f3838f2b11acaae136487b7
Namdar S <namdarsadiq@gmail.com>
2023-07-13 04:25:41 -0400

    request handling all done

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
 
-module.exports = nextConfig
+nextConfig.rewrites = async () => {
+  return [
+    {
+      source: '/api/ocr',
+      destination: 'http://localhost:8502/predict/',
+    },
+  ];
+};
+
+module.exports = nextConfig;
--- src/app/api/valid/route.ts
+++ src/app/api/valid/route.ts
@@ -1,5 +1,5 @@
-import { NextRequest, NextResponse } from 'next/server';
 import { Configuration, OpenAIApi } from 'openai';
+import { NextRequest, NextResponse } from 'next/server';
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -7,21 +7,24 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 export async function POST(req: NextRequest) {
-  const { prompt } = await req.json();
-
   try {
+    const { latexCode } = await req.json();
+
+    const prompt = `The following text may or may not be valid latex code: ${latexCode}}
+                    process it so that it's valid latex code that can be rendered.
+                    Do not return anything in your response but the actual latex code.`;
     const completion = await openai.createCompletion({
       model: 'text-davinci-003',
-      prompt: `give me latex code for ${prompt}. 
-               Do not return anything in your response but the actual latex code.`,
+      prompt: `${prompt}`,
       max_tokens: 50,
     });
 
-    const result = { text: completion.data.choices[0].text };
-    return new NextResponse(JSON.stringify(result), {
+    const latexCodeValid = { text: completion.data.choices[0].text };
+    return new NextResponse(JSON.stringify(latexCodeValid), {
       headers: { 'Content-Type': 'application/json' },
     });
   } catch (error: any) {
+    console.log('error', error);
     return new NextResponse(error.message || error.toString(), {
       status: error.status || 500,
       headers: { 'Content-Type': 'application/json' },
