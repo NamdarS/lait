c1174e9605e0f0e1471f0064e753d7bce8752cea
Namdar S <namdarsadiq@gmail.com>
2023-08-21 23:57:46 -0400

    search bar stylings

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -14,10 +14,10 @@ export const TextInput: React.FC = () => {
     'rounded',
     'px-3',
     'py-2',
-    'w-1/3',
+    'w-2/5',
     'focus:outline-none',
     'focus:ring',
-    'focus:ring-blue-200',
+    'focus:ring-blue-500',
     'focus:ring-opacity-50',
     'shadow',
   ].join(' ');
